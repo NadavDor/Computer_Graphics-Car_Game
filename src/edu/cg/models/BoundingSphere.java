3ca062780e6e9b180bd8eaebb0101a70d1cf6ce6
Nadav Dor <nadavdor274@gmail.com>
2020-07-02 12:20:50 +0300

    Initial commit

---
+++ src/edu/cg/models/BoundingSphere.java
@@ -0,0 +1,11 @@
+package edu.cg.models;
+
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.glu.GLU;
+import com.jogamp.opengl.glu.GLUquadric;
+
+import edu.cg.algebra.Point;
+
+public class BoundingSphere implements IRenderable {
+	// TODO Add your implementation
+}
---
+++ src/edu/cg/models/Car/F1Car.java
@@ -0,0 +1,14 @@
+package edu.cg.models.Car;
+
+import com.jogamp.opengl.*;
+
+import edu.cg.models.IIntersectable;
+import edu.cg.models.IRenderable;
+
+/**
+ * A F1 Racing Car.
+ *
+ */
+public class F1Car implements IRenderable, IIntersectable {
+        // TODO: Put your implementation from previous exercise.
+        }
